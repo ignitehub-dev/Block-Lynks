1b1e8ab9f26be33162b000f16b6576a618ecc469
Minniti-Paul <Minniti_Paul@bah.com>
2019-11-11 11:04:33 -0500

    dapp token init

---
+++ Dapp Token/contracts/Token.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.11;
+
+contract Token {
+    // Constructor
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+
+    // Set the total number of tokens
+    // Get the total number of tokens
+}
